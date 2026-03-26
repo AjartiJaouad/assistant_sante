a2db83ceca9eac981ebe1f9ddfd611cd82f18899
ajartijaouad <ajartijaouad14@gmail.com>
2026-03-26 14:19:40 +0100

    docs: add global swagger info and security scheme

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,31 @@
 <?php
 
 namespace App\Http\Controllers;
-
+/**
+ * @OA\Info(
+ * version="1.0.0",
+ * title="Health Assistant API Documentation",
+ * description="Documentation de l'API Assistant Santé avec IA (Laravel 12)",
+ * @OA\Contact(
+ * email="support@healthapi.com"
+ * )
+ * )
+ *
+ * @OA\Server(
+ * url=L5_SWAGGER_CONST_HOST,
+ * description="API Server"
+ * )
+ *
+ * @OA\SecurityScheme(
+ * type="http",
+ * description="Utilisez un token Sanctum (Bearer)",
+ * name="Authorization",
+ * in="header",
+ * scheme="bearer",
+ * bearerFormat="JWT",
+ * securityScheme="bearerAuth",
+ * )
+ */
 abstract class Controller
 {
     //
